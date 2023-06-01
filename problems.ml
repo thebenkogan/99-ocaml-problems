d60362195abee9abdf1f911017f2aa6bd50465aa
Ben Kogan <benkogan9@gmail.com>
2023-06-01 17:21:13 -0400

    next 3

--- problems.ml
+++ problems.ml
@@ -14,3 +14,16 @@ let rec nth lst n =
   match lst with
   | [] -> failwith "nth"
   | h :: t -> if n = 0 then h else nth t (n - 1)
+
+let rec length = function
+  | [] -> 0
+  | _ :: t -> 1 + length t
+
+let rev lst =
+  let rec rev_aux acc = function
+    | [] -> acc
+    | h :: t -> rev_aux (h :: acc) t
+  in
+  rev_aux [] lst
+
+let is_palindrome lst = lst = rev lst
